a1659bfa1cb1ae8919bffe1b3932f8f166209452
Kamran <iskama98@mail.ru>
2021-03-25 20:51:31 +0300

    add

---
+++ Unit/js/script.js
@@ -0,0 +1,61 @@
+//Task-
+//document.querySelector('.one').innerHTML = 'Hello World';
+
+//Task-
+document.querySelector('span').innerHTML = 'world';
+
+//Task-10
+document.querySelector('h3').innerHTML = 'Task-10';
+
+//Task-11
+let h4 = document.querySelector('.four');
+h4.innerHTML = '<h4> Task-11 </h4>';
+h4.innerHTML += '<p> Done! </p>';
+
+//Task-12
+let a = document.querySelector('.five');
+a.innerHTML = 3.1415;
+
+//Task-13
+let div7 = document.querySelector('.seven');
+div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
+
+//Task-14
+let z1 = 6;
+let z2 = 3;
+let eight = document.querySelector('.eight');
+
+eight.innerHTML = z1 * z2;
+
+//Task-15
+let y1 = 6;
+let y2 = 3;
+let nine = document.querySelector('.nine');
+
+nine.innerHTML = y1 / y2;
+
+//Task-16
+let x1 = 'Hello';
+let x2 = 5;
+
+document.querySelector('.ten').innerHTML = x1 + x2;
+
+//Task-17
+let d1 = document.querySelector('.test-1');
+
+console.log(d1);
+
+//Task-18
+let d2 = document.querySelector('.test-2');
+
+console.log(d2);
+
+d2 = 5;
+console.log(d2);
+
+//Task-19
+let divS3 = document.querySelector('.s3');
+console.log(divS3);
+
+divS3 = document.querySelector('.s4');
+console.log(divS3);
