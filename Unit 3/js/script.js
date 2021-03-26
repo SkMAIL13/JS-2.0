ac1054c58c981e27b043ced68b115e5d65c02e13
Kamran <iskama98@mail.ru>
2021-03-26 21:12:15 +0300

    Start Unit-3

---
+++ Unit 3/js/script.js
@@ -0,0 +1,24 @@
+//Task-1
+const i1 = document.querySelector('.i-1');
+const b1 = document.querySelector('.b-1');
+const out1 = document.querySelector('.out-1');
+
+b1.onclick = function f1 () {
+   if ( i1.value == '4' ) {
+      out1.innerHTML = 'true';
+   } else {
+      out1.innerHTML = 'false';
+   }
+  
+}
+
+//Task-2
+let a21 = 21;
+let a22 = 22;
+const b2 = document.querySelector('.b-2');
+
+b2.onclick = function f2 () {
+   if (a21 == a22) {
+     
+   }
+}
