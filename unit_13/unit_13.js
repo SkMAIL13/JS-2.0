0259ed5703dd4b149d5ff978599ad26f0470a47d
Kamran <iskama98@mail.ru>
2021-04-17 22:49:58 +0300

    Complete Task-6

--- unit_13/unit_13.js
+++ unit_13/unit_13.js
@@ -111,13 +111,18 @@ document.querySelector('.b-5').onclick = () => {
 // Task 6
 // Добавьте input .i-61 и i-62. При нажатии b-6 выполняете функцию f6. Функция должна получать из i-61 ключ, а из i-62 значение и добавлять его в массив a6. После этого, с помощью функции f5 выводите массив a6 в out-6.
 
+
 let a6 = {
     "b": 17,
     "e": 22
 };
 
 function f6() {
+    let i61 = document.querySelector('.i-61').value;
+    let i62 = document.querySelector('.i-62').value;
 
+    a6[i61] = i62;
+    f5(a6, '.out-6');
 }
 
 document.querySelector('.b-6').onclick = f6;
