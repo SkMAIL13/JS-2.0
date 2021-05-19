fe8f22810c81071b35dc119add8f4f24c1da9d56
Kamran <iskama98@mail.ru>
2021-05-19 16:11:05 +0300

    feat: complete unit-27

--- unit 27/unit_27.js
+++ unit 27/unit_27.js
@@ -7,10 +7,32 @@
     должна по нажатию b-1.</p>
 */
 
-function t1() {
+function content(one, two, out) {
+    Promise.all([one, two]).then(value => {
+        document.querySelector(out).textContent = value;
+    })
 }
 
+function t1() {
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=1')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+
+    let b = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=2&name=Kamran')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content (a, b, '.out-1')
+}
+
+
 // ваше событие здесь!!!
+document.querySelector('.b-1').onclick = t1
 
 // Task 2 ============================================
 /* 
@@ -26,11 +48,24 @@ function t1() {
 */
 
 function t2() {
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=3&num1=111&num2=666')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
 
+    let b = new Promise((resolve,reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=3&num1=111&num2=555')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content(a, b, '.out-2')
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-2').onclick = t2
 
 // Task 3 ============================================
 /*  
@@ -43,14 +78,27 @@ function t2() {
 <p>Два
 запроса объедините с помощью promiseAll.
 Выведите в out-3 результат. Запускаться функция должна по нажатию b-3. </p>
-                 */
+*/
 
 function t3() {
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=5')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
 
+    let b = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=6&num1=1&num2=10')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content(a, b, '.out-3')
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-3').onclick = t3
 
 // Task 4 ============================================
 /*  
@@ -66,10 +114,24 @@ function t3() {
 */
 
 function t4() {
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=7')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
 
+    let b = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=8&year=1998')
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content(a, b, '.out-4')
 }
 
 // ваше событие здесь!!!
+document.querySelector('.b-4').onclick = t4
 
 // Task 5 ============================================
 /*  
@@ -77,15 +139,42 @@ function t4() {
 <p>Отправьте
 POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 2. </p>
 <p>Два
-запроса объедините с помощью promiseAll. Результат выведите в out-5 результат. Запускаться функция
+запроса объедините с помhtp://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=2&name=Kamtranощью promiseAll. Результат выведите в out-5 результат. Запускаться функция
 должна по нажатию b-5.</p>
 */
 
 function t5() {
-
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=1', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'auth=DdC33D7d2C2a7&action=1',
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+        
+    })
+            
+    let b = new Promise((resolve,reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=2&name=Kamran', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'auth=DdC33D7d2C2a7&action=2&name=Kamran'
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content(a, b, '.out-5')
 }
 
 // ваше событие здесь!!!
+document.querySelector('.b-5').onclick = t5
 
 // Task 6 ============================================
 /* 
@@ -103,11 +192,36 @@ function t5() {
 */
 
 function t6() {
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=3&num1=111&num2=777', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'auth=DdC33D7d2C2a7&action=3&num1=111&num2=666'
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+    })
 
+    let b = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=4&num1=0&num2=100', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'auth=DdC33D7d2C2a7&action=4&num1=0&num2=100'
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content(a, b, '.out-6')
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-6').onclick = t6
 
 // Task 7 ============================================
 /*  
@@ -122,10 +236,36 @@ function t6() {
 */
 
 function t7() {
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=5', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'auth=DdC33D7d2C2a7&action=5'
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+    })
 
+    let b = new Promise((resolve,reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=6&num1=0&num2=10', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+            body: 'auth=DdC33D7d2C2a7&action=6&num1=0&num2=10'
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content(a, b, '.out-7')
 }
 
 // ваше событие здесь!!!
+document.querySelector('.b-7').onclick = t7
 
 // Task 8 ============================================
 /* 
@@ -141,8 +281,33 @@ function t7() {
 */
 
 function t8() {
+    let a = new Promise((resolve, reject) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=7', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'auth=DdC33D7d2C2a7&action=7'
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+    })
 
+    let b = new Promise((resolve, rejcet) => {
+        fetch('http://getpost.itgid.info/index2.php?auth=DdC33D7d2C2a7&action=8&year=1998', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'auth=DdC33D7d2C2a7&action=8&year=1998'
+        })
+        .then(data => {
+            resolve(data.text())
+        })
+    })
+    content(a, b, '.out-8')
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-8').onclick = t8
