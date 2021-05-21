c9d8d9f4cd0336d8fd44467d322aba529637f2f4
Kamran <iskama98@mail.ru>
2021-05-21 21:45:34 +0300

    feat: goods2 draw

--- unit 28/Goods2.js
+++ unit 28/Goods2.js
@@ -1,9 +1,17 @@
-class Goods2 extends Goods{
-   constructor(name, amount, image, sale, count){
-     super(name, amount, image, count);
-     this.sale = sale;
-   }
-   draw(domElement){
-     document.querySelector(domElement).innerHTML += `<div><span class="material-icons">${this.image}</span> ${this.name} ${this.amount} sale: ${this.sale}</div>` 
-   }
- }
+class Goods2 extends Goods {
+  constructor(name, amount, image, count, price, out, sale) {
+      super(name, amount, image, count, price, out);
+      this.sale = sale;
+  }
+  draw() {
+      if (this.sale === true) {
+      document.querySelector(this.out).innerHTML += `<div><span class="material-icons">
+      ${this.image} 
+      </span>${this.name} <br>total-prise  <strong>${this.amount * this.price}</strong><br>sale`;}
+      else  {
+          document.querySelector(this.out).innerHTML += `<div><span class="material-icons">
+          ${this.image}
+          </span>${this.name}<br>total-prise  <strong>${this.amount * this.price}</strong>`;
+      }
+  }
+}
