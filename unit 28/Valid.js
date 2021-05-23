06300ef6f4987e011c32b91c0b7c868a4d027656
Kamran <iskama98@mail.ru>
2021-05-23 16:04:12 +0300

    refactor: delete comments

--- unit 28/Valid.js
+++ unit 28/Valid.js
@@ -1,5 +1,3 @@
-//Cоздайте класс Valid, который содержит свойства email, password, isValid. И метод validate. Метод validate должен проверять длину пароля и писать false в isValid если длина меньше 6 и true если больше. Изначально свойство isValid равно false
-
 class Valid {
    constructor (email, password, isValid, validate) {
       this.email = email;
