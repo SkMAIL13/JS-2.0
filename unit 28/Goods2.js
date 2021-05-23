501bf2a55b2ee1f038314e94ce0470ddd5fe6a7c
Kamran <iskama98@mail.ru>
2021-05-23 15:40:46 +0300

    fix: fix code

--- unit 28/Goods2.js
+++ unit 28/Goods2.js
@@ -1,17 +1,18 @@
+
 class Goods2 extends Goods {
-  constructor(name, amount, image, count, price, out, sale) {
-      super(name, amount, image, count, price, out);
-      this.sale = sale;
-  }
-  draw() {
-      if (this.sale === true) {
-      document.querySelector(this.out).innerHTML += `<div><span class="material-icons">
-      ${this.image} 
-      </span>${this.name} <br>total-prise  <strong>${this.amount * this.price}</strong><br>sale`;}
-      else  {
-          document.querySelector(this.out).innerHTML += `<div><span class="material-icons">
-          ${this.image}
-          </span>${this.name}<br>total-prise  <strong>${this.amount * this.price}</strong>`;
-      }
-  }
+    constructor(name, amount, image, count, price, out, sale) {
+        super(name, amount, image, count, price, out);
+        this.sale = sale;
+    }
+    draw() {
+        if (this.sale === true) {
+        document.querySelector(this.out).innerHTML += `<div><span class="material-icons">
+        ${this.image}
+        </span>${this.name}<br>total-price  <strong>${this.amount * this.price}</strong><br>sale`;}
+        else  {
+            document.querySelector(this.out).innerHTML += `<div><span class="material-icons">
+            ${this.image}
+            </span>${this.name}<br>total-price  <strong>${this.amount*this.price}</strong>`;
+        }
+    }
 }
